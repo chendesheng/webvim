d5caa863ea069dffaa1bce65c0f8da602937a4c9
chendesheng <ichendesheng@gmail.com>
2019-08-05 21:01:30 +0800

    Add logo

--- src/Boot.elm
+++ src/Boot.elm
@@ -4,10 +4,12 @@ import Browser.Dom as Dom
 import Font exposing (FontInfo, measureFont, renderMeasureDivs)
 import Helper.Helper exposing (httpErrorMessage)
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as HtmlAttr exposing (..)
 import Http
 import Json.Decode as Decode
 import Json.Encode as Encode
+import Svg exposing (..)
+import Svg.Attributes as SvgAttr
 import Task
 import Url exposing (Url)
 import Url.Parser as UrlParser
@@ -104,14 +106,66 @@ sendBoot service =
 -- view
 
 
+forbidden =
+    g []
+        [ circle
+            [ SvgAttr.cx "100"
+            , SvgAttr.cy "100"
+            , SvgAttr.r "60"
+            , SvgAttr.style "fill:none;stroke:darkred;stroke-width:20"
+            , SvgAttr.strokeOpacity "0.5"
+            ]
+            []
+        , line
+            [ SvgAttr.x1 "50"
+            , SvgAttr.y1 "100"
+            , SvgAttr.x2 "150"
+            , SvgAttr.y2 "100"
+            , SvgAttr.style "fill:none;stroke:darkred;stroke-width:20;"
+            , SvgAttr.transform "rotate(45, 100, 100)"
+            , SvgAttr.strokeOpacity "0.5"
+            ]
+            []
+        ]
+
+
+logo title animation failed =
+    div
+        [ class "logo"
+        , classList [ ( "logo-initializing", animation ) ]
+        , HtmlAttr.title title
+        ]
+        [ svg [ height 200, width 200 ]
+            ([ polygon [ SvgAttr.points "0,0 0,100 100,100", SvgAttr.style "fill:rgb(127,178,198);" ] []
+             , polygon [ SvgAttr.points "0.5,0 0.5,100 100,100", SvgAttr.style "fill:none;stroke:black;" ] []
+             , polygon [ SvgAttr.points "200,0 100,100 200,100", SvgAttr.style "fill:rgb(92,99,116);" ] []
+             , polygon [ SvgAttr.points "199.5,0 100,100 199.5,100", SvgAttr.style "fill:none;stroke:black;" ] []
+             , polygon [ SvgAttr.points "0,100 50,100 50,150", SvgAttr.style "fill:rgb(225,175,83);stroke:black;" ] []
+             , polygon [ SvgAttr.points "50,100 100,100 100,150 50,150", SvgAttr.style "fill:rgb(58,202,101);stroke:black;" ] []
+             , polygon [ SvgAttr.points "100,100 150,100 100,150", SvgAttr.style "fill:rgb(225,175,83);stroke:black;" ] []
+             , polygon [ SvgAttr.points "150,100 100,150 150,150 200,100", SvgAttr.style "fill:rgb(58,202,101);stroke:black;" ] []
+             , polygon [ SvgAttr.points "50,150 150,150 100,200", SvgAttr.style "fill:rgb(127,178,198);stroke:black;" ] []
+             ]
+                ++ (if failed then
+                        [ forbidden ]
+
+                    else
+                        []
+                   )
+            )
+        ]
+
+
 view : Model -> Html msg
 view model =
     case model of
         Booting _ ->
-            renderMeasureDivs
+            div []
+                [ renderMeasureDivs, logo "Initializing" True False ]
 
         BootFailed message ->
-            text <| "Boot failed: " ++ message
+            div []
+                [ logo message False True ]
 
 
 
